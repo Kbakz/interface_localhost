fc680c4472bd64fc2fce2089405316c949432f18
Kbakz <68775535+Kbakz@users.noreply.github.com>
2021-10-18 00:31:12 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php 
 include('Systema/index.php');
-define("INCLUDE_PATH", __DIR__);
-define("INCLUDE_PATH_STATIC",INCLUDE_PATH.'Systema/Class/Views');
-\Class\Controllers\CriarNovoController::criarProjeto();
 \Class\Views\RenderView::exibir();
+\Class\Controllers\CriarNovoController::criarProjeto();
 ?>
 
