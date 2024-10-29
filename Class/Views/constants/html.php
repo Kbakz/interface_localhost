2151720309f4ebeabac8ee4023d36c5e76395c24
Kevin Freire <68775535+Kbakz@users.noreply.github.com>
2024-10-29 17:14:16 -0300

    l

--- Class/Views/constants/html.php
+++ Class/Views/constants/html.php
@@ -1,8 +1,9 @@
 <?php 
 $htmlContent = '<!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>'.$newFolder.'</title>
 	<link rel="stylesheet" href="'.$urlCss.'">
 </head>
